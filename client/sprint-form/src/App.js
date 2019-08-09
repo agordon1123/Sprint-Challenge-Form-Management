69979249076f3c2b905e58c292c3b2673506ef34
Alex <agordon1123@gmail.com>
2019-08-09 13:17:41 -0400

    get request rendered

--- client/sprint-form/src/App.js
+++ client/sprint-form/src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm';
+import Axios from 'axios';
+import UserList from './components/UserList';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World from App</h1>
-      <LoginForm />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = []
+  };
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  getUser = () => {
+    Axios.get('http://localhost:5000/api/restricted/data')
+      .then(res => {
+        console.log('getUser res: ', res)
+        this.setState(res.data)
+      })
+      .catch(err => {
+        console.log('getUser err: ', err)
+      })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Create a login to access our secret recipes:</h1>
+        <LoginForm />
+        <UserList props={this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/sprint-form/src/components/UserCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const UserCard = (props) => {
+    console.log("props in UserCard", props.props.props)
+    const userArray = Object.values(props.props.props);
+    console.log(userArray);
+    return(
+        <div className='user-list'>
+            {userArray.map(user => {
+                return (
+                    <div className='user-card'>
+                        <p>Name: {user.name}</p>
+                        <p>Course: {user.course}</p>
+                        <p>Technique: {user.technique}</p>
+                        <p>Ingredients: {user.ingredients}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default UserCard;
